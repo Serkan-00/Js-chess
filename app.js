9041b3499e86cae89dbc9759c4150fef49c3d69a
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-15 16:33:21 +0100

    Able to drag&drop

--- app.js
+++ app.js
@@ -41,3 +41,34 @@ function createBoard() {
 }
 
 createBoard()
+
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart(e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragDrop(e) {
+    e.stopPropagation()
+
+    //e.target.parentNode.append(draggedElement)
+    e.target.append(draggedElement)
+
+}
+
