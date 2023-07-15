3df74b63e9f2ddf87ba2bb23dc23a695eb4d6bbb
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-15 14:08:47 +0100

    King and Queen image

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-document.querySelector ("#gameboard")
+const gameBoard = document. querySelector ("#gameboard")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+
+const startPieces = [
+
+]
