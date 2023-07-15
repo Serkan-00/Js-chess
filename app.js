6e300a44a5d7a4eb6e126e9298188a42da2b44c0
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-15 14:24:56 +0100

    Array of pieces

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const width = 8
 
 
 const startPieces = [
-
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
+
