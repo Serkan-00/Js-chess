295cb690b6918d02aede23ed4ab4cbbe36a8a864
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-15 14:40:27 +0100

    chess board

--- app.js
+++ app.js
@@ -15,3 +15,14 @@ const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
 
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.setAttribute('square-id', i)
+        square.classList.add('beige')
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
