cb2ec21bf3c67551a6863fbc5cfaa3cb6cedcf66
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-20 16:08:13 +0100

    game complete

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ function dragDrop(e) {
        if (takenByOpponent && valid) {
            e.target.parentNode.append(draggedElement)
            e.target.remove()
+           checkForWin()
            changePlayer()
            return
        }
@@ -92,6 +93,7 @@ function dragDrop(e) {
 
         if (valid) {
             e.target.append(draggedElement)
+            checkForWin()
             changePlayer()
             return
         }
@@ -316,4 +318,18 @@ function reverseIds() {
 function revertIds() {
     const allSquares = document.querySelectorAll(".square")
     allSquares.forEach((square, i) => square.setAttribute('square-id', i))
+}
+
+function checkForWin() {
+    const kings = Array.from(document.querySelectorAll('#king'))
+    if (!kings.some(king => king.firstChild.classList.contains('white'))) {
+        infoDisplay.innerHTML = "Black player wins!"
+        const allSquares = document.querySelectorAll('.square')
+        allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
+    }
+    if (!kings.some(king => king.firstChild.classList.contains('black'))) {
+        infoDisplay.innerHTML = "Black player wins!"
+        const allSquares = document.querySelectorAll('.square')
+        allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
+    }
 }
